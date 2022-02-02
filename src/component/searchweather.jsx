3d76f372a5755c0d9362167f9ed05ec3bb469318
deadjin929 <85065570+deadjin929@users.noreply.github.com>
2022-02-02 11:04:54 +0500

    first upload

---
+++ src/component/searchweather.jsx
@@ -0,0 +1,108 @@
+import React, { useEffect, useState } from "react";
+
+export default function Searchweather() {
+  const [search, setSearch] = useState("Islamabad");
+  const [data, setData] = useState([]);
+  const [input, setInput] = useState("");
+
+  let componentMounted = true;
+
+  useEffect(() => {
+    const fetchWeather = async () => {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=2c7cc6e3eff1d346cadab1eff86e331d`
+      );
+      if (componentMounted) {
+        setData(await response.json());
+        console.log(data);
+      }
+      return () => {
+        componentMounted = false;
+      };
+    };
+    fetchWeather();
+  }, [search]);
+
+  let emoji = null;
+  if (typeof data.main != "undefined") {
+    if (data.weather[0].main == "Clouds") {
+      emoji = "fa-cloud";
+    } else if (data.weather[0].main == "Thunderstorm") {
+      emoji = "fa-bolt";
+    } else if (data.weather[0].main == "Drizzle") {
+      emoji = "fa-cloud-rain";
+    } else if (data.weather[0].main == "Rain") {
+      emoji = "fa-cloud-shower-heavy";
+    } else if (data.weather[0].main == "Snow") {
+      emoji = "fa-snow-flake";
+    } else if (data.weather[0].main == "Clear") {
+      emoji = "fa-sun";
+    } else {
+      emoji = "fa-smog";
+    }
+  } else {
+    return <div>..loading</div>;
+  }
+
+  let temp = (data.main.temp - 273.15).toFixed(2);
+  let temp_min = (data.main.temp_min - 273.15).toFixed(2);
+  let temp_max = (data.main.temp_max - 273.15).toFixed(2);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setSearch(input);
+  };
+
+  return (
+    <div>
+      <div className="container mt-5">
+        <div className="row justify-content-center">
+          <div className="col-md-4">
+            <div class="card text-center border-0 text-white">
+              <img
+                src={`https://source.unsplash.com/600x900/?${data.weather[0].main}`}
+                class="card-img"
+                alt="..."
+              />
+              <div class="card-img-overlay">
+                <form onSubmit={handleSubmit}>
+                  <div class="input-group mb-4 w-80 mx-auto">
+                    <input
+                      type="text"
+                      class="form-control"
+                      placeholder="Search City"
+                      aria-label="Search City"
+                      aria-describedby="basic-addon2"
+                      name="search"
+                      value={input}
+                      onChange={(e) => setInput(e.target.value)}
+                      required
+                    />
+                    <button
+                      type="submit"
+                      class="input-group-text"
+                      id="basic-addon2"
+                    >
+                      <i className="fas fa-search"></i>
+                    </button>
+                  </div>
+                </form>
+                <div className="bg-dark bg-opacity-50 py-3">
+                  <h2 class="card-title">{data.name}</h2>
+                  <p class="card-text-lead">Wenesday, Feb 2,2022</p>
+                  <hr />
+                  <i className={`fas ${emoji} fa-3x`}></i>
+                  <h1 className="fw-border mb-5">{temp}&deg;C</h1>
+                  <p className=" lead fw-border mb-1">{data.weather[0].main}</p>
+                  <p className=" lead">
+                    {temp_min}&deg;C | {temp_max}&deg;C
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
